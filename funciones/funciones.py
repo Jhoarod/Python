528f2bb393f4933919506b109c74bf2cfb3bfc3f
Jhoan Rodriguez <jhoan.azul.7@gmail.com>
2026-04-02 23:58:37 -0500

    order the functions

---
+++ funciones/funciones.py
@@ -0,0 +1,14 @@
+
+#fUNCION TYPE
+var = 1
+print("Tipo de dato", type(var))
+
+
+# Suma de listas
+print("Suma de listas:", sum([1,2,3]))
+print("Suma de listas:", sum(list((1,2,3))))
+
+# Suma de diccionarios
+print("suma de diccionarios:", sum(dict(((1,"Uno"),(2,"dos"), (3,"tres")))))
+
+print("adicion:", sum([]) )
