6102717fed21b9ee22693500866db854285b5390
Jhoan Rodriguez <jhoan.azul.7@gmail.com>
2026-07-17 23:12:06 -0500

    omit the md file

--- estructuras.py
+++
@@ -1,9 +0,0 @@
-conjunto = set()
-print(conjunto)
-
-conjunto.add(5)
-print(conjunto)
-
-
-conjunto.add(1)
-print(conjunto)
--- operations.py
+++
@@ -1,6 +0,0 @@
-num_1 = 2 
-num_2 = 3
-
-
-print("Suma:", num_1 + num_2)
-print("resta", num_1 - num_2) 
